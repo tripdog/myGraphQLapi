23027643d3d47fd6f2a719ccd31146764a42e209
Tom Phillips <eflips@yahoo.com>
2021-06-07 16:43:24 -0700

    trying to find error cannot get /

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const express = require("express")
+const express = require("express");
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
 //requer the schema file
-const schema = require('./schema.js')
+const schema = require("./schema.js");
+
 //initialize the express server
-const app = express()
+const app = express();
 
 //Now add the middleware and route it
-app.use('/graphql', graphqlHTTP({
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     graphiql: true,
-}))
+  })
+);
 
 app.listen(3000, () => {
-    console.log("Server is ready Cap'n")
-})
-
+  console.log("Server is ready Captain!");
+});
--- schema.js
+++ schema.js
@@ -1,19 +1,19 @@
-const graphql = require("graphql")
-/* Note the below declaration is the object destructuring syntax */
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = graphql
+const graphql = require("graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = graphql;
 
 //Initialize the schema value
 const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-       name: 'RootQueryType',
-        fields: {
-            hello: {
-                type: GraphQLString,
-                resolve() {
-                    return "muthafuckas"
-                }
-           }
-       }
-    })
-})
-module.exports = schema
+  query: new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return "muthafuckas";
+        },
+      },
+    },
+  }),
+});
+
+module.exports = schema;
