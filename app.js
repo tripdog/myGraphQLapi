6e3ab39d8ed81e8e1b813fe65bdfb7a1b4014c97
Tom Phillips <eflips@yahoo.com>
2021-06-07 16:13:05 -0700

    enable graphiql

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express")
+//requer the schema file
+const schema = require('./schema.js')
 //initialize the express server
 const app = express()
 
 //Now add the middleware and route it
-app.use('/graphql', graphqlHTTP())
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}))
 
 app.listen(3000, () => {
     console.log("Server is ready Cap'n")
